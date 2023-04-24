68bb9f45416df90b63adc74013dee4ea32059312
Saprou <mostafaelsaprout@gmail.com>
2023-04-24 07:05:23 +0200

    d\

---
+++ lib/AzureBlobStorageHandler.php
@@ -0,0 +1,47 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Blue32a\Monolog\Handler;
+
+use MicrosoftAzure\Storage\Blob\BlobRestProxy;
+use Monolog\Handler\AbstractProcessingHandler;
+use Monolog\Logger;
+use Monolog\LogRecord;
+
+class AzureBlobStorageHandler extends AbstractProcessingHandler
+{
+    /** @var BlobRestProxy */
+    protected $client;
+
+    /** @var string */
+    protected $container;
+
+    /** @var string */
+    protected $blob;
+
+    /**
+     * @param int|string $level
+     */
+    public function __construct(
+        BlobRestProxy $client,
+        string $container,
+        string $blob,
+        $level = Logger::DEBUG,
+        bool $bubble = true
+    ) {
+        $this->client    = $client;
+        $this->container = $container;
+        $this->blob      = $blob;
+
+        parent::__construct($level, $bubble);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    protected function write(LogRecord $record): void
+    {
+        $this->client->appendBlock($this->container, $this->blob, $record['formatted']);
+    }
+}
