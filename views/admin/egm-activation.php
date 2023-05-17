29cd6026fac8751eef9d2051d43a31e87c0d0361
Saprou <mostafaelsaprout@gmail.com>
2023-05-18 01:00:32 +0300

    e

--- views/admin/egm-activation.php
+++ views/admin/egm-activation.php
@@ -250,20 +250,24 @@
 <script>
     const ctx = document.getElementById('myPieChart');
     let Piedata = {
-        datasets: [{
-            data: [10, 20, 30]
-        }],
-
-        // These labels appear in the legend and in the tooltips when hovering different arcs
         labels: [
             'Red',
-            'Yellow',
-            'Blue'
-        ]
-    };
+            'Blue',
+            'Yellow'
+        ],
+        datasets: [{
+            label: 'My First Dataset',
+            data: [300, 50, 100],
+            backgroundColor: [
+                'rgb(255, 99, 132)',
+                'rgb(54, 162, 235)',
+                'rgb(255, 205, 86)'
+            ],
+            hoverOffset: 4
+        }]
+    };;
     new Chart(ctx, {
         type: 'doughnut',
         data: Piedata,
     });
-
 </script>
